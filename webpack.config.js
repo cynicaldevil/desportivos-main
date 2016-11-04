082c715446eaa1ae2fbf5f219c1bf158dc580e17
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-11-05 02:38:17 +0530

    Added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    context: __dirname,
+    entry: {
+        'main': './index.js',
+    },
+    output: {
+        path: __dirname + 'dist/assets',
+        filename: '[name].bundle.js',
+        publicPath: '/assets'
+    },
+    devServer: {
+        contentBase: __dirname,
+    },
+    devtool: 'eval-source-map',
+    module: {
+        rules: [
+            {  
+                test: /\.jsx|\.js$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015', 'react']
+                    }
+                }]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    }
+};
