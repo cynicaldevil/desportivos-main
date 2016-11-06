b9c6297707afd633503c8f476fbd8b5d8ced5c43
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-11-07 04:21:13 +0530

    Added basic hexagon code

--- components/Home.jsx
+++ components/Home.jsx
@@ -8,19 +8,52 @@ class Home extends React.Component {
     }
     render() {
         const styles = {
-            color: '#000',
-            ':hover': {
-                backgroundColor: '#0074d9'
+            hexagon_wrapper: {
+                textAlign: 'center',
+                margin: 20,
+                height: 100,
+                width: 100,
+                position: 'relative',
+                display: 'inline-block',
+                transform: 'rotateZ(90deg)'
+            },
+            hexagon: {
+                height: '100%',
+                width: (100 * 0.57735) + '%',
+                display: 'inline-block',
+                backgroundColor: '#556474',
+            },
+            hexagon_before: {
+                position: 'absolute',
+                top: 0,
+                right: (100 / 2) - ((100 * 0.57735) / 2) + '%',
+                backgroundColor: '#556474',
+                height: 'inherit',
+                width: (100 * 0.57735) + '%',
+                content: '',
+                transform: 'rotateZ(60deg)'
+            },
+            hexagon_after: {
+                position: 'absolute',
+                top: 0,
+                right: (100 / 2) - ((100 * 0.57735) / 2) + '%',
+                backgroundColor: '#556474',
+                height: 'inherit',
+                width: (100 * 0.57735) + '%',
+                content: '',
+                transform: 'rotateZ(-60deg)'
             }
         };
-        return(
-            <div>
-                <p style={{color: 'blue'}} >Hello World!</p>
-                <button style={styles}>Button</button>
-                <Button bsStyle="primary">Primary</Button>
+        return (
+            <div style={{backgroundColor: '#ecf0f1'}}>
+                <div style={styles.hexagon_wrapper} >
+                    <div style={styles.hexagon_before} />
+                    <div style={styles.hexagon} ></div>
+                    <div style={styles.hexagon_after} />
+                </div>
             </div>
         );
-    }
+  }
 }
 
 export default Radium(Home);
