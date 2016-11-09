6c90b78c46bbaef42dec783dec6526d3807d92e1
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-11-09 20:44:55 +0530

    Removed babel stage 3 preset and installed stage 0

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
                 use: [{
                     loader: 'babel-loader',
                     options: {
-                        presets: ['es2015', 'react', 'stage-3']
+                        presets: ['es2015', 'react', 'stage-0']
                     }
                 }]
             },
