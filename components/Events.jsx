21465569ec0382eb6956313f144be48aeaeeec56
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-11-09 20:54:10 +0530

    Added code to load scripts

--- components/Events.jsx
+++ components/Events.jsx
@@ -196,9 +196,85 @@ class Events extends React.Component {
         `;
         return html_str;
     }
+
+    componentDidMount() {
+
+        // IMP TODO: Fix effect on hover to show canvas and wireframe images
+
+        const script_paths = [
+            'js/events/modernizr.custom.js',
+            '../js/events/imagesloaded.pkgd.min.js',
+            '../js/events/masonry.pkgd.min.js',
+            '../js/events/dynamics.min.js',
+            '../js/events/classie.js',
+            '../js/events/animOnScroll.js',
+            '../js/events/main.js'
+        ];
+
+        script_paths.forEach((script_path) => {
+            const script = document.createElement("script");
+            script.type  = 'text/javascript';
+            script.src = script_path;
+            this.scriptDiv.appendChild(script);
+        });
+
+        // for inline js
+        const inline_js = `(function() {
+                function getRandomInt(min, max) {
+                    return Math.floor(Math.random() * (max - min + 1)) + min;
+                }
+
+                [].slice.call(document.querySelectorAll('.isolayer')).forEach(function(el) {
+                    new IsoGrid(el, {
+                        type : 'scrollable',
+                        transform : 'translateX(-15vw) translateY(275px) rotateX(45deg) rotateZ(45deg)',
+                        stackItemsAnimation : {
+                            properties : function(pos) {
+                                return {
+                                    translateZ: (pos+1) * 50,
+                                    rotateZ: getRandomInt(-3, 3)
+                                };
+                            },
+                            options : function(pos, itemstotal) {
+                                return {
+                                    type: dynamics.bezier,
+                                    duration: 500,
+                                    points: [{"x":0,"y":0,"cp":[{"x":0.2,"y":1}]},{"x":1,"y":1,"cp":[{"x":0.3,"y":1}]}],
+                                    //delay: (itemstotal-pos-1)*40
+                                };
+                            }
+                        },
+                        onGridLoaded : function() {
+                            classie.add(document.body, 'grid-loaded');
+                        }
+                    });
+                });
+            })();
+        `;
+        const inline_js_script = document.createElement("script");
+        inline_js_script.type = 'text/javascript';
+        inline_js_script.text = inline_js;
+
+        let flag = false;
+        let counter = 0;
+        let isDefined = window.setInterval(() => {
+            if(!(typeof IsoGrid === "undefined") && !flag) {
+                this.scriptDiv.appendChild(inline_js_script);
+                flag = true;
+            }
+            counter += 100;
+        }, 100);
+
+        // automatically stop interval after 3 seconds
+        window.setTimeout(() => {
+            clearInterval(isDefined);
+        }, 3000);
+    }
+
     render() {
         return(
             <div>
+                <div ref={(input) => this.scriptDiv = input} />
                 <p>Events</p>
                 <div dangerouslySetInnerHTML={{__html: this.createMarkup()}} />
             </div>
