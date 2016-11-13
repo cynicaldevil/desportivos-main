b3486b676ff427edfe764a28f75ab60f33e1d536
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-11-13 15:43:17 +0530

    Added image component fro gallery

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
+class Image extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        const data = this.props.data;
+        return(
+            <div className="grid__item" data-size={data.dimensions}>
+                <a href={data.image} className="img-wrap"><img src={data.thumb} alt={data.alt} />
+                    <div className="description description--grid">Felicity</div>
+                </a>
+            </div>
+        );
+    }
+}
+
 class Gallery extends React.Component {
     constructor(props) {
         super(props);
