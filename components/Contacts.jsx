cac9fc80b552789ce3cb47c663fbe8fa4b98c209
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-11-26 23:51:29 +0530

    Added basic 'contact' component

--- components/Contacts.jsx
+++ components/Contacts.jsx
@@ -1,5 +1,33 @@
 import React from 'react';
 
+require('../css/contacts/contacts.css');
+
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="grid">
+                {/* TODO: Move this link tag to another page */}
+                <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+                <figure className="effect-zoe">
+                    <img src="../img/contacts/26.jpg" alt="img26"/>
+                    <figcaption>
+                        <h2>Creative <span>Zoe</span></h2>
+                        <p className="icon-links">
+                            <a href="#"><i className="material-icons">accessible</i></a>
+                            <a href="#"><i className="material-icons">all_out</i></a>
+                            <a href="#"><i className="material-icons">autorenew</i></a>
+                        </p>
+                        <p className="description">Zoe never had the patience of her sisters. She deliberately punched the bear in his face.</p>
+                    </figcaption>
+                </figure>
+            </div>
+        );
+    }
+}
+
 class Contacts extends React.Component {
     constructor(props) {
         super(props);
@@ -7,7 +35,7 @@ class Contacts extends React.Component {
     render() {
         return(
             <div>
-                <p>Contacts</p>
+                <Contact />
             </div>
         );
     }
