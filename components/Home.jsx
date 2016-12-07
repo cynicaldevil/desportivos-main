d34127fd9090f873d8e9e4bd28384f4fc5130337
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-12-08 03:08:44 +0530

    Added config for particles

--- components/Home.jsx
+++ components/Home.jsx
@@ -108,12 +108,136 @@ class Home extends React.Component {
             },
             honeycomb: {
                 marginLeft: '50%',
+            },
+            particles: {
+                position: 'absolute',
+                top: 0, left: 0,
+                border: '1px solid white',
+                backgroundColor: '#ffe5b6',
+                zIndex: -1,
+            },
             }
         };
+
+        const particles_config = {
+            particles: {
+                number: {
+                    value: 154,
+                    density: {
+                        enable: true,
+                        value_area: 800
+                    }
+                },
+                color: {
+                    value: '#e8a429'
+                },
+                shape: {
+                    type: 'circle',
+                    stroke: {
+                        width: 0,
+                        color: '#000000'
+                    },
+                    polygon: {
+                        nb_sides: 5
+                    },
+                    image: {
+                        src: 'img/github.svg',
+                        width: 100,
+                        height: 100
+                    }
+                },
+                opacity: {
+                    value: 0.5,
+                    random: false,
+                    anim: {
+                        enable: false,
+                        speed: 1,
+                        opacity_min: 0.1,
+                        sync: false
+                    }
+                },
+                size: {
+                    value: 4,
+                    random: true,
+                    anim: {
+                        enable: false,
+                        speed: 40,
+                        size_min: 0.1,
+                        sync: false
+                    }
+                },
+                line_linked: {
+                    enable: true,
+                    distance: 150,
+                    color: '#f0b752',
+                    opacity: 0.4,
+                    width: 1
+                },
+                move: {
+                    enable: true,
+                    speed:6,
+                    direction: "none",
+                    random: false,
+                    straight: false,
+                    out_mode: "out",
+                    bounce: false,
+                    attract: {
+                        enable: false,
+                        rotateX: 600,
+                        rotateY: 1200
+                    }
+                }
+              },
+              interactivity: {
+                detect_on: 'canvas',
+                events: {
+                    onhover: {
+                        enable: true,
+                        mode: 'repulse'
+                    },
+                    onclick: {
+                        enable: true,
+                        mode: 'push'
+                    },
+                    resize: true
+                },
+                modes: {
+                    grab: {
+                        distance: 400,
+                        line_linked: {
+                        opacity: 1
+                    }
+                },
+                    bubble: {
+                        distance: 400,
+                        size: 40,
+                        duration: 2,
+                        opacity: 8,
+                        speed: 3
+                },
+                    repulse: {
+                        distance: 154.29467033725558,
+                        duration: 0.4
+                    },
+                    push: {
+                        particles_nb: 4
+                    },
+                    remove: {
+                        particles_nb: 2
+                  }
+                }
+              },
+              retina_detect: true
+        };
+
         const length = 120;
         return (
-                <Particles style={{position: 'absolute', top: 0, left: 0}}/>
             <div style={{paddingTop: 94}}>
+                <Particles width={'100%'}
+                           height={'100%'}
+                           style={styles.particles}
+                           params={particles_config}
+                />
                 <div style={styles.honeycomb}>
                     <div style={styles.row}>
                         <Hexagon length={length}
