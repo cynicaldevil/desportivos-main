e33b44d8a4555a48390049880f5f52d72ccf291b
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-12-08 21:20:22 +0530

    removed border and changed height of root

--- components/Root.jsx
+++ components/Root.jsx
@@ -8,11 +8,11 @@ class Root extends React.Component {
     }
     render() {
         return(
-            <div style={{border: '1px solid black'}} >
-            {!(this.props.children.type.name === 'RadiumEnhancer') && <Navbar />}
-            <div style={{height: '100vh'}}>
-                {this.props.children}
-            </div>
+            <div>
+                {!(this.props.children.type.name === 'RadiumEnhancer') && <Navbar />}
+                <div style={{height: '100%'}}>
+                    {this.props.children}
+                </div>
             </div>
         );
     }
