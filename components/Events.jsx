ad6056a5d13dfc3665c8c31e9c989f101e92aa9d
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-12-10 21:06:28 +0530

    Added scrollhelper, and some additional styles to Event

--- components/Events.jsx
+++ components/Events.jsx
@@ -19,8 +19,16 @@ class Sport extends React.Component {
                 }
             }
         };
+        const show_styles = () => {
+            if(typeof this.props.show !== 'undefined' && !this.props.show) {
+                return {
+                    visibility: 'hidden'
+                }
+            }
+            else return styles.main;
+        };
         return (
-            <li className="grid__item" style={styles.main}>
+            <li className="grid__item" style={show_styles()}>
                 <div className="grid__link" onClick={() => this.props.onClickCB(this.props.name) }>
                     <img className="grid__img layer" src="../img/events/back_1.png" alt="Canvas Dummy" />
                     <img className="grid__img layer" src="../img/events/back_2.png" alt="Dummy" />
@@ -81,6 +89,52 @@ class SportsDataPanel extends React.Component {
     );
   }
 }
+
+const ScrollHelper = (props) => {
+    const styles = {
+        main: {
+            marginRight: 70,
+            marginTop: 70,
+            padding: 10,
+            float: 'right',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'flex-start',
+            flexDirection: 'row',
+            backgroundColor: 'rgba(255, 255, 255, 0.3)',
+            borderTop: '7px solid white',
+        },
+        text: {
+            fontSize: 55,
+            color: '#FFF',
+            textTransform: 'uppercase',
+            fontFamily: `"Oswald", sans-serif`,
+            margin: 20,
+            lineHeight: 1,
+        },
+        arrow: {
+            marginTop: -20,
+            marginRight: 20,
+            marginLeft: -20,
+        },
+        line: {
+            width: '95%',
+            height: 3,
+            backgroundColor: 'white',
+            marginRight: 100
+        }
+    };
+    return(
+        <div style={styles.main}>
+            <link href="https://fonts.googleapis.com/css?family=Oswald:700" rel="stylesheet" />
+            <img style={styles.arrow} src='../img/events/down_arrow.svg' alt='arrow' />
+            <div style={{marginBottom: 20}}>
+                <div style={styles.text}>Scroll <br />to <br />view <br /> more</div>
+                <div style={styles.line} />
+            </div>
+        </div>
+    );
+};
 
 class Events extends React.Component {
     constructor(props) {
@@ -298,8 +352,32 @@ class Events extends React.Component {
                     />;
         });
 
+        // hack: insert invisible items so that the it scrolls all the way to the end
+        let count = 0;
+        while(count < 3) {
+            display_sports.push(<Sport show={false} key={display_sports.length}/>);
+            count++;
+        }
+
+        const styles = {
+            main: {
+                paddingTop: 94,
+                backgroundColor: '#E0DEFF',
+                backgroundSize: 'cover',
+            },
+            background: {
+                width: '100vw',
+                height: '100vh',
+                zIndex: -1,
+                position: 'absolute',
+                top: 0,
+                left: 0,
+
+            }
+        };
+
         return(
-            <div style={{backgroundColor: '#CAFF25', paddingTop: 94}}>
+            <div style={styles.main}>
                 <div ref={(input) => this.scriptDiv = input} />
                 <div className="include-deps">
                     <link rel="stylesheet" type="text/css" href="../css/events/normalize.css" />
@@ -310,6 +388,7 @@ class Events extends React.Component {
                                                         data={this.state.panel_data}
                                                         onModalCloseCB={this.onModalCloseCB}
                                                     />}
+                    <ScrollHelper />
                     <div className="isolayer isolayer--scroll1 isolayer--shadow">
                         <ul className="grid grid--effect-flip">
                             {display_sports}
