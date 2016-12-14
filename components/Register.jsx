6ae150c975ad407877de7e1f881f82b6e6ec4d20
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-12-14 23:42:10 +0530

    Added Register form

--- components/Register.jsx
+++ components/Register.jsx
@@ -6,8 +6,9 @@ class Register extends React.Component {
     }
     render() {
         return(
-            <div style={{paddingTop: 94}}>
-                <p>Register</p>
+            <div style={{paddingTop: 94, height: '100vh', overflow: 'hidden'}}>
+                <iframe id="typeform-full" width="100%" height="100%" frameBorder="0" src="https://dikshantrathi.typeform.com/to/bXIe3W"></iframe>
+                <script type="text/javascript" src="https://s3-eu-west-1.amazonaws.com/share.typeform.com/embed.js"></script>
             </div>
         );
     }
